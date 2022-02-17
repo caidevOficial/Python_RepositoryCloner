608e1be133bf5869c99b4a34731d9298ea922552
CaidevOficial <falcone.facu@gmail.com>
2022-02-16 22:48:59 -0300

    Update main files to V2.1.12

--- GithubCloner2022.py
+++ GithubCloner2022.py
@@ -15,48 +15,75 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import datetime
 import pandas as pd
 
 from Modules.DataFrameHandler_Mod.DFHandler import DataFrameHandler as DfH
-from Modules.GetData_Mod.DataManager import DataManager as DM
+from Modules.DataManager_Mod.DataManager import DataManager as DM
+from Modules.Formatter_Mod.Formatter import Formatter as FMT
+from Modules.PrintMessage_Mod.CloneMessenger import CloneMessenger as CM
 
 # ?######### Start Basic Configuration ##########
 filename = 'Github_Repositories.csv'
 name = 'Github Repository Cloner'
-version = '[V2.0.11]'
+version = '[V2.0.12]'
 author = '[FacuFalcone - CaidevOficial]'
 fileConfigName = 'Modules/API_Info.json'
 # ?######### End Basic Configuration ##########
 
-try:
-    # ?#########? Start Initialization ##########
-    JsonFile = pd.read_json(f"./{fileConfigName}", orient='records')
-    JsonAPI = JsonFile['Github']
-    JsonDFConfigs = JsonFile['DataFrame']['Fields']
-    # ?#########? End Initialization ############
+if __name__ == '__main__':
 
-    # ?#########? Start Objects Instances ##########
-    Handler = DfH()
-    Manager = DM()
-    # ?#########? End Objects Instances ##########
+    start_time = datetime.datetime.now()
+    
+    try:
+        # ?#########? Start Initialization ##########
+        JsonFile = pd.read_json(f"./{fileConfigName}", orient='records')
+        JsonAPI = JsonFile['Github']
+        JsonDFConfigs = JsonFile['DataFrame']['Fields']
+        # ?#########? End Initialization ############
 
-    # ?#########? Start DataManager Configuration ##########
-    Manager.InitialConfig(name, version, author, JsonAPI)
-    # ?#########? End DataManager Configuration ##########
+        # ?#########? Start Objects Instances ##########
+        Handler = DfH()
+        Manager = DM()
+        Messenger = CM()
+        Timer = FMT()
+        # ?#########? End Objects Instances ##########
 
-    # ?#########? Start DataFrame Configuration ##########
-    # *# Reads the 'csv' File to get the dataframe
-    df = pd.read_csv(filename)
+        # ?#########? Start DataManager Configuration ##########
+        Manager.InitialConfig(name, version, author, JsonAPI)
+        # ?#########? End DataManager Configuration ##########
 
-    # *# Sets the Main DF to the class to handle it
-    Handler.MainDataFrame = df
-    Handler.ConfigsJsonValues = JsonDFConfigs
-    Handler.ConfigurateDataFrame(Handler.ConfigsJsonValues['Course'])
-    # ?#########? End DataFrame Configuration ##########
+        # ?#########? Start DataFrame Configuration ##########
+        # *# Reads the 'csv' File to get the dataframe
+        df = pd.read_csv(filename)
 
-    # ?#########? Start Initialize DataManager ##########
-    Manager.CloneRepositories(Handler)
-    # ?##########? End Initialize DataManager ###########
+        # *# Sets the Main DF to the class to handle it
+        Handler.MainDataFrame = df
+        Handler.ConfigsJsonValues = JsonDFConfigs
+        Handler.ConfigurateDataFrame(Handler.ConfigsJsonValues['Course'])
+        # ?#########? End DataFrame Configuration ##########
 
-except Exception as e:
-    print(f'Exception: {e.args}')
+        # ?#########? Start Initialize DataManager ##########
+        Manager.CloneRepositories(Handler)
+        # ?##########? End Initialize DataManager ###########
+
+        
+    except Exception as e:
+        print(f'Exception: {e.args}')
+    finally:
+        # ?#########? Start Timer Config ##########
+        Timer.CrudeTime = start_time
+        # ?#########? End Timer Config ##########
+
+        # ?#########? Start Print Message ########## 
+        Messenger.Message = f"Elapsed Time: {Timer.FormattedTimeStr}"
+        Messenger.PrintMessage()
+
+        Messenger.Message = f"Thanks for using {name} {version} by {author}! ♥"
+        Messenger.PrintMessage()
+
+        Messenger.Message = "Success! All task done. Press a key to close the app"
+        Messenger.PrintMessage()
+        # ?#########? End Print Message ##########
+
+        end = input()
